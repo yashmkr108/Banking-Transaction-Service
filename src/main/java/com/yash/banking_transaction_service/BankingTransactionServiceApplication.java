9c9cf2d254baa4d2c843eba6de6efe212594439c
Yash Makkar <yash.mkr108@gmail.com>
2026-09-14 14:49:30 +0530

    chore: bootstrap banking transaction service

---
+++ src/main/java/com/yash/banking_transaction_service/BankingTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yash.banking_transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionServiceApplication.class, args);
+	}
+
+}
